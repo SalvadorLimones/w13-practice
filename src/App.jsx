40ed0f3741f3230220bbea4e6c0b7ee7182db239
SalvadorLimones <87721843+SalvadorLimones@users.noreply.github.com>
2022-02-02 15:38:34 +0100

    wednesday fun

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,110 @@
 import { useState } from "react";
 import "./App.css";
 
+const Post = ({ post, editPost, deletePost }) => {
+  const [newTitle, setNewTitle] = useState(post.title);
+  const [newText, setNewText] = useState(post.postText);
+  const [isEditing, setEditing] = useState(false);
+  return isEditing ? (
+    <div key={post.id}>
+      <input
+        className="title-edit"
+        onChange={(e) => setNewTitle(e.target.value)}
+        value={newTitle}
+        type="text"
+        placeholder="new post title"
+      />
+      <textarea
+        className="text-edit"
+        onChange={(e) => setNewText(e.target.value)}
+        value={newText}
+        placeholder="new post t"
+      />
+
+      <p>{post.date.toLocaleString()}</p>
+
+      <button
+        onClick={() =>
+          editPost(post.id, newTitle, newText, isEditing, setEditing)
+        }
+      >
+        submit change
+      </button>
+    </div>
+  ) : (
+    <div key={post.id}>
+      <h2>{post.title}</h2>
+      <p>{post.postText}</p>
+      <p>{post.date.toLocaleString()}</p>
+      <button onClick={() => setEditing(true)}>edit</button>
+      <button onClick={() => deletePost(post.id)}>delete</button>
+    </div>
+  );
+};
+
 function App() {
   const [posts, setPosts] = useState([]);
+  const [title, setTitle] = useState("");
+  const [postText, setPostText] = useState("");
 
-  function addNewPost() {
+  const addNewPost = () => {
     setPosts([
       ...posts,
       {
-        title: "Random",
-        postText: "Szöveg4",
-        date: "2022.02.02",
+        title: title,
+        postText: postText,
+        date: new Date(),
+        id: Math.random(),
       },
     ]);
-  }
+    setTitle("");
+    setPostText("");
+  };
+
+  const deletePost = (deleteThis) =>
+    setPosts(posts.filter((post) => post.id !== deleteThis));
+
+  const editPost = (id, newTitle, newText, isEditing, setEditing) => {
+    const newList = [];
+    for (let post of posts) {
+      if (post.id === id) {
+        post.title = newTitle;
+        post.postText = newText;
+      }
+      newList.push(post);
+    }
+    setPosts(newList);
+    setEditing(false);
+  };
 
   return (
     <div className="App">
       <h1>Blog Posts</h1>
       <div>
-        <input type="text" placeholder="title" />
-        <input type="textarea" placeholder="post" />
+        <input
+          onChange={(e) => setTitle(e.target.value)}
+          type="text"
+          placeholder="title"
+          value={title}
+        />
+        <input
+          onChange={(e) => setPostText(e.target.value)}
+          type="textarea"
+          placeholder="post"
+          value={postText}
+        />
         <button onClick={addNewPost}>add post</button>
         <button onClick={() => setPosts([])}>delete all</button>
       </div>
 
-      {posts.map((post) => {
-        return (
-          <div>
-            <h2>{post.title}</h2>
-            <p>{post.postText}</p>
-            <p>{post.date}</p>
-            <input type="text" placeholder="new post text" />
-            <button>edit</button>
-            <button>delete</button>
-          </div>
-        );
-      })}
+      {posts.map((post) => (
+        <Post
+          post={post}
+          key={post.id}
+          editPost={editPost}
+          deletePost={deletePost}
+        />
+      ))}
     </div>
   );
 }
